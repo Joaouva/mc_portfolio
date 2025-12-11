7cfdd57408fed983165a578f749b5dceba13741a
JoaoUva <joaouva@gmail.com>
2025-12-11 09:39:44 +0000

    fix github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/mc_portfolio',
   images: {
     unoptimized: true,
     remotePatterns: [
